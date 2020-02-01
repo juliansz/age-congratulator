e673e08ff2f17959c16607a57a5ddcc3d30250e0
Julian <julian@getapp.com.ar>
2020-02-01 00:04:29 -0300

    Updates

---
+++ precache-manifest.73f49294f448a79070893ed8e097bd18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887f23c6d1309ed48125b7e2a579e4f6",
+    "url": "/age-congratulator/index.html"
+  },
+  {
+    "revision": "b8f9124cb59df1ac2bf5",
+    "url": "/age-congratulator/static/css/main.5c762a8e.chunk.css"
+  },
+  {
+    "revision": "f5bc7aec3936f4d15e45",
+    "url": "/age-congratulator/static/js/2.3b8ce822.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/age-congratulator/static/js/2.3b8ce822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8f9124cb59df1ac2bf5",
+    "url": "/age-congratulator/static/js/main.34d45bb5.chunk.js"
+  },
+  {
+    "revision": "597fde8071f1116711cc",
+    "url": "/age-congratulator/static/js/runtime-main.f1bdc536.js"
+  },
+  {
+    "revision": "c222bf9ed4b952db2259dd39f98a97a1",
+    "url": "/age-congratulator/static/media/congrats.c222bf9e.gif"
+  },
+  {
+    "revision": "32b00a426425d23831bec0b8a4de0f97",
+    "url": "/age-congratulator/static/media/error.32b00a42.gif"
+  }
+]);
